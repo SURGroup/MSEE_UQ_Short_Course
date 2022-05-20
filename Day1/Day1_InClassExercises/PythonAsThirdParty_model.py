f46b97040429ca1d100ac7dc110d4a1146451f93
Dimitris Tsapetis <dimtsap@hotmail.com>
2022-05-20 11:30:34 -0400

    Add files via upload

---
+++ Day1/Day1_InClassExercises/PythonAsThirdParty_model.py
@@ -0,0 +1,15 @@
+import fire
+import os
+from os.path import dirname
+import os.path
+from os import path
+
+def run_model(index):
+    # Model is run here using CLI 
+    # Runs are performed in run/{index} folder
+    pass
+
+if __name__=='__main__':
+    fire.Fire(run_model)
+
+
---
+++ Day1/Day1_InClassExercises/process_3rd_party_output.py
@@ -0,0 +1,6 @@
+import numpy as np
+import meshio
+
+def read_output(index):
+    #Here you can read output files generated from the model runs
+    pass
